5f4df226bde91336d5cd2b95ae756f5a41ab4118
dheom <dhcolab365@gmail.com>
2025-04-20 22:39:58 +0900

    홈스크린 이미지 UI 제작 및

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -25,23 +25,62 @@ class HomeScreen extends StatelessWidget {
         builder: (context, snapshot) {
           //snapshot은 futurebuilder의 결과값을 담고있음
           if (snapshot.hasData) {
-            return ListView.separated(
-              //ListView.separated는 ListView.builder와 비슷하지만 아이템사이에 구분선(위젯)을 넣을수있음
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                //한번에 로딩하지않고 필요한 만큼만 로딩함
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => SizedBox(
-                width: 20,
-              ),
+            return Column(
+              children: [
+                const SizedBox(height: 20),
+                Expanded(child: makeList(snapshot)),
+              ],
             );
           }
-          return Center(child: CircularProgressIndicator()); //로딩중일때 보여줄 위젯
+          return Expanded(child: CircularProgressIndicator()); //로딩중일때 보여줄 위젯
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      //ListView.separated는 ListView.builder와 비슷하지만 아이템사이에 구분선(위젯)을 넣을수있음
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      //활용법으로 만약 날짜로 최근 일주일 간의 글만 볼수있도록 할수있음
+      //무료회원:메인화면에서 3일전까지의 글만 볼수있음
+      //유료회원:메인화면에서 7일전까지의 글만 볼수있음
+      //근데 이건 서버에서는 다 받아오는거니까 서버측에서 처리하는게 유리할듯
+      itemBuilder: (context, index) {
+        //한번에 로딩하지않고 필요한 만큼만 로딩함
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.4),
+                    blurRadius: 15,
+                    offset: Offset(10, 10),
+                  ),
+                ],
+              ),
+              child: Image.network(
+                webtoon.tumb,
+                headers: const {'Referer': 'https://comic.naver.com'},
+                //fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              webtoon.title,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => SizedBox(width: 40),
     );
   }
 }
